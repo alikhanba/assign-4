93c405b95befc24a307d5ebb70c2af8dddd6379a
keons <fsomething180@proton.me>
2026-02-06 11:12:23 +0500

    changed the project with the new one,used for restapi

--- src/main/java/com/example/inventory_system/controller/product_controller.java
+++ src/main/java/com/example/inventory_system/controller/product_controller.java
@@ -7,21 +7,17 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController // 2. Tells Spring this class handles web requests
-@RequestMapping("/api") // 3. All your URLs will start with http://localhost:8080/api
+@RestController
+@RequestMapping("/api")
 public class product_controller {
 
-    // 4. Create an instance of your Inventory class to talk to the DB
     private Inventory inventory = new Inventory("Main Warehouse", 500);
 
-    // 5. This handles: http://localhost:8080/api/products
     @GetMapping("/products")
     public List<Product> getAll() {
-        // This calls the method you refactored in the Inventory class!
         return inventory.getProducts();
     }
 
-    // 6. This handles searching: http://localhost:8080/api/products/search/Laptop
     @GetMapping("/products/search/{name}")
     public Product search(@PathVariable String name) {
         return inventory.getProductbyname(name);
@@ -32,4 +28,6 @@ public class product_controller {
         inventory.addProduct(newProduct);
         return "Product added successfully!";
     }
+
+
 }
